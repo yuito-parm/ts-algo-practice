ec55e6166c9267b1758e546963e1ac31c8dcefcd
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-28 18:08:37 +0900

    配列内の最大値を見つける関数

--- 02_findMax.ts
+++ 02_findMax.ts
@@ -1,5 +1,5 @@
 const nums: number[] = [1, 7, 3, 5];
 
-const findMax = (nums: number[]) => nums.reduce((max, curr) => max > curr ? max : curr, nums[0])
+const findMax = (nums: number[]):number => nums.reduce((max, curr) => max > curr ? max : curr, nums[0])
 
 console.log(findMax(nums));
